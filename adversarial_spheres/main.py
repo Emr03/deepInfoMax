6fbeb64cc26db36209574d837955dd09d1800e43
elsinator <elsa.riachi@mail.mcgill.ca>
2020-03-23 22:33:14 -0400

    minor fixes in adversarial spheres

--- adversarial_spheres/main.py
+++ adversarial_spheres/main.py
@@ -9,14 +9,14 @@ def generate(batch_size, R, d):
     norm = torch.norm(x, p=2, dim=-1, keepdim=True).repeat(1, d)
 
     # sample binary labels and scale accordingly
-    y = torch.randint(size=batch_size, low=0, high=2)
+    y = torch.randint(size=(batch_size, 1), low=0, high=2) * 1.0
     x = x + (R - 1) * y * x # if y = 0 x, else R * x
     return x, y
 
 def get_model(input_dim):
     return nn.Sequential(nn.Linear(input_dim, 1000),
                          nn.ReLU(),
-                         nn.BatchNorm1d(1000).
+                         nn.BatchNorm1d(1000),
                          nn.Linear(1000, 1000),
                          nn.ReLU(),
                          nn.BatchNorm1d(1000),
@@ -24,11 +24,13 @@ def get_model(input_dim):
 
 def train(input_dim=500, steps=1000000, batch_size=50, R=1.3):
 
-    model = get_model(input_dim=input_dim)
+    model = get_model(input_dim=input_dim).cuda()
     loss_fn = nn.BCEWithLogitsLoss()
     opt = torch.optim.Adam(lr=0.0001, params=model.parameters())
     for t in range(steps):
         X, Y = generate(batch_size, R, input_dim)
+        X = X.cuda()
+        Y = Y.cuda()
         loss = loss_fn(model(input=X), Y)
         opt.zero_grad()
         loss.backward()
@@ -38,4 +40,7 @@ def train(input_dim=500, steps=1000000, batch_size=50, R=1.3):
             print("Step {}: loss {}".format(t, loss))
 
     torch.save(model.state_dict(), f="adv_spheres_model.pth")
+
+if __name__ == "__main__":
+    train()
 
