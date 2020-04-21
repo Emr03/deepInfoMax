70209499d35689e4a4970f395a07c7e791586e9f
Emr03 <elsa.riachi@mail.mcgill.ca>
2020-04-21 19:58:20 -0400

    updated visualize

--- visualize.py
+++ visualize.py
@@ -25,8 +25,8 @@ def visualize_attacks(classifier, decoder, X, Y, args):
     """
 
     # pass clean X through classifier's encoder, get clean representations
-    C_clean, E_clean = classifier.encoder(X)
-    print(E_clean.shape)
+    Z_clean = classifier.encoder(X, intermediate=True)
+    print(Z_clean.shape)
 
     # compute adversarial perturbations and save
     X_adv, delta, out, out_adv = pgd(model=classifier, X=X, y=Y, epsilon=args.epsilon,
@@ -42,28 +42,31 @@ def visualize_attacks(classifier, decoder, X, Y, args):
     plt.show()
 
     # pass perturbed input through classifier's encoder, get perturbed representations
-    C_adv, E_adv = classifier.encoder(X_adv)
+    Z_adv = classifier.encoder(X_adv, intermediate=True)
 
     X_adv = ((X_adv * std + mean) * 255).int()
     plt.imshow(X_adv[0].permute(1, 2, 0))
     plt.show()
 
-    # X_hat = decoder(E_clean.unsqueeze(0))
+    # X_hat = decoder(E_clean)
     # X_hat = ((X_hat * std + mean) * 255).int()
     # plt.imshow(X_hat[0].permute(1, 2, 0))
     # plt.show()
-
-    # X_hat_adv = decoder(E_adv.unsqueeze(0))
+    #
+    # X_hat_adv = decoder(E_adv)
     # X_hat_adv = ((X_hat_adv * std + mean) * 255).int()
     # plt.imshow(X_hat_adv[0].permute(1, 2, 0))
     # plt.show()
 
     # compare clean vs pert representations (L2 norm, difference per dimension, are some dimensions consistently unchanged?)
-    C_l2 = torch.frobenius_norm(C_adv - C_clean, dim=(-1, -2))
-    E_l2 = torch.norm(E_clean - E_adv, p=2, dim=-1, keepdim=True)
+    Z_l2 = torch.norm(Z_clean - Z_adv, p=2, dim=-1, keepdim=True)
+    print("Z_l2", Z_l2, Z_clean, Z_adv)
 
-    print("C_l2", C_l2)
-    print("E_l2", E_l2)
+    # reshape E x E
+    E_diff = torch.abs((Z_adv - Z_clean)).reshape(-1, 32, 32)
+    plt.matshow(E_diff[0].detach().numpy())
+    plt.show()
+
     print(out, out_adv)
 
 if __name__ == "__main__":
@@ -91,17 +94,20 @@ if __name__ == "__main__":
         classifier = ClassifierY(encoder=encoder, hidden_units=args.hidden_units, num_classes=10)
 
     # load classifier from checkpoint
-    classifier.load_state_dict(torch.load("classifier_supervised/classifier_supervised_checkpoint.pth",
+    # classifier.load_state_dict(torch.load("classifier_supervised_new/classifier_supervised_new_checkpoint.pth",
+    #                                       map_location=torch.device("cpu"))["classifier_state_dict"])
+
+        classifier.load_state_dict(torch.load("classifier_fc_jsd_prior/classifier_fc_jsd_prior_checkpoint.pth",
                                           map_location=torch.device("cpu"))["classifier_state_dict"])
 
     # decoder = DecoderY(input_size=encoder.output_size)
-    # decoder.load_state_dict(torch.load("checkpoints/cifar10/decoders/cifar10_decoding_checkpoint.pth",
+    # decoder.load_state_dict(torch.load("decoder_jsd_new/decoder_jsd_new_checkpoint.pth",
     #                                    map_location=torch.device("cpu"))["decoder_state_dict"])
     # decoder.eval()
 
     classifier = classifier.to(args.device)
 
-    evaluation.evaluate_adversarial(args, model=classifier, loader=test_loader)
+    #evaluation.evaluate_adversarial(args, model=classifier, loader=test_loader)
     for X, Y in test_loader:
         visualize_attacks(classifier, None, X, Y, args)
 
