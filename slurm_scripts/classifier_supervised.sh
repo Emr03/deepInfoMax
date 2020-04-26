542556bce39cfb957c177bb8b1d903bc1b1d26e4
Emr03 <elsa.riachi@mail.mcgill.ca>
2020-04-26 16:48:45 -0400

    updated slurm script

--- slurm_scripts/classifier_supervised.sh
+++ slurm_scripts/classifier_supervised.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-bash slurm_scripts/launch_slurm_job.sh t4 classifier_supervised 1 \
-    "python3 classification.py --encoder_stride 2 --fully_supervised --gpu \
-    --prefix classifier_supervised_200 --weight_decay=0.0001 --hidden_units=200 --batch_size=64 --lr=0.0001 --dropout=0.1 --epochs=2000"
+for mode in "fc" "conv" "y"; do
+ bash slurm_scripts/launch_slurm_job.sh t4 classifier_supervised_${mode} 1 \
+     "python3 classification.py --encoder_stride 2 --fully_supervised --gpu --input_layer ${mode} \
+     --prefix classifier_supervised_${mode} --weight_decay=0.0001 --hidden_units=200 --batch_size=64 \
+     --lr=0.0001 --dropout=0.1 --epochs=2000"
