448aabe68eb1ab35d7bc869e7b2f4e0d2fb96b57
elsinator <elsa.riachi@mail.mcgill.ca>
2020-04-27 11:04:51 -0400

    updated slurm scripts

--- slurm_scripts/classifier_supervised.sh
+++ slurm_scripts/classifier_supervised.sh
@@ -5,3 +5,4 @@ for mode in "fc" "conv" "y"; do
      "python3 classification.py --encoder_stride 2 --fully_supervised --gpu --input_layer ${mode} \
      --prefix classifier_supervised_${mode} --weight_decay=0.0001 --hidden_units=200 --batch_size=64 \
      --lr=0.0001 --dropout=0.1 --epochs=2000"
+done
--- slurm_scripts/classifier_sweep.sh
+++ slurm_scripts/classifier_sweep.sh
@@ -11,7 +11,7 @@ for dir in experiments/local_infomax_encoder_*; do
     # partition, j_name, resource cmd
     bash slurm_scripts/launch_slurm_job.sh p100  classifier_${mode}_${dir} 1 \
     "python3 classification.py --encoder_stride ${stride} --encoder_ckpt=${encoder_ckpt} --input_layer ${mode} --gpu \
-                  --prefix classifier_${mode}_${dir} --batch_size=64 --hidden_units=200 --dropout=1.0 --weight_decay=0.0001"
+                  --prefix classifier_${mode}_${dir} --batch_size=64 --hidden_units=200 --dropout=0.1 --lr=0.0001"
   done
 done
 
