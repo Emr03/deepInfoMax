be25fe46e3eba4c4b3778430174854fb32a840cd
elsinator <elsa.riachi@mail.mcgill.ca>
2020-04-27 16:25:06 -0400

    updates to slurm scripts from vremote

--- slurm_scripts/classifier_supervised.sh
+++ slurm_scripts/classifier_supervised.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for mode in "fc" "conv" "y"; do
- bash slurm_scripts/launch_slurm_job.sh t4 classifier_supervised_${mode} 1 \
+for mode in "fc" "y"; do
+ bash slurm_scripts/launch_slurm_job.sh p100 classifier_supervised_${mode} 1 \
      "python3 classification.py --encoder_stride 2 --fully_supervised --gpu --input_layer ${mode} \
-     --prefix classifier_supervised_${mode} --weight_decay=0.0001 --hidden_units=200 --batch_size=64 \
-     --lr=0.0001 --dropout=0.1 --epochs=2000"
+     --prefix classifier_supervised_${mode}_1024 --hidden_units=1024 --batch_size=128 \
+     --lr=0.0001 --dropout=0.0 --epochs=1000"
 done
