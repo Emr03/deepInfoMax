f78b5a0c9fdc44d194f9033eb9049810185ea708
elsinator <elsa.riachi@mail.mcgill.ca>
2020-04-30 17:59:44 -0400

    updated slurm script

--- slurm_scripts/classifier_sweep.sh
+++ slurm_scripts/classifier_sweep.sh
@@ -8,10 +8,11 @@ for dir in experiments/local_infomax_encoder_*; do
     dir=${dir/experiments\//}
     echo ${dir}
     encoder_ckpt=experiments/${dir}/${dir}_checkpoint.pth
+    classifier_ckpt=classifier_${mode}_${dir}_adv_200_checkpoint.pth
     # partition, j_name, resource cmd
     bash slurm_scripts/launch_slurm_job.sh gpu adv_classifier_${mode}_${dir} 1 \
-    "python3 classification.py --encoder_stride ${stride} --encoder_ckpt=${encoder_ckpt} --input_layer ${mode} --gpu \
-                  --prefix classifier_${mode}_${dir}_adv_200 --classifier_adversarial --batch_size=64 --hidden_units=200 --dropout=0.1 --lr=0.0001"
+    "python3 classification.py --encoder_stride ${stride} --classifier_ckpt=${classifier_ckpt} --input_layer ${mode} --gpu \
+                  --prefix classifier_${mode}_${dir}_adv_200 --classifier_adversarial --batch_size=64 --hidden_units=200 --dropout=0.1 --lr=0.0001 --epochs=2000"
   done
 done
 
