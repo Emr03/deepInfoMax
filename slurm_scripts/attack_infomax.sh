3a42889a16051d7f443cdef9b75ede201b5ee1a6
elsinator <elsa.riachi@mail.mcgill.ca>
2020-05-12 15:51:43 -0400

    slurm_scripts/attack_infomax.sh

--- slurm_scripts/attack_infomax.sh
+++ slurm_scripts/attack_infomax.sh
@@ -4,10 +4,11 @@ mode=fc
 mi_estimator=nce
 stride=2
 for dir in experiments/infomax_classifiers/classifier_${mode}_local_infomax_encoder_${mi_estimator}_*; do
-name=${dir\/experiments\/infomax_classifiers\/}
-classifier_ckpt=${dir}/${name}_checkpoint.pth
-encoder_name=${name\/_${mode}_}
-encoder_ckpt=experiments/encoders/prior/${encoder_name}/${encoder_name}_checkpoint.pth
-bash slurm_scripts/launch_slurm_job.sh gpu attack_infomax 1 \
+ name=${dir/experiments\/infomax_classifiers\/}
+ classifier_ckpt=${dir}/${name}_checkpoint.pth
+ encoder_name=${name/classifier_${mode}_}
+ encoder_ckpt=experiments/encoders/prior/${encoder_name}/${encoder_name}_checkpoint.pth
+ bash slurm_scripts/launch_slurm_job.sh gpu attack_infomax 1 \
     "python3 attack_infomax.py --classifier_ckpt=${classifier_ckpt} --encoder_ckpt=${encoder_ckpt} --input_layer ${mode} \
-    --gpu --mi_estimator ${mi_estimator} --prefix ${dir\/experiments}  --batch_size=64"
+    --gpu --mi_estimator ${mi_estimator} --prefix ${dir/experiments}  --batch_size=64"
+done
